3760ad37f2fc498d8fc95074d7d331a64041cbe5
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-07-05 16:58:21 +0200

    Homepage h1 style

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <the-homepage></the-homepage>
+    <about-me></about-me>
+  </div>
+</template>
+
+<script>
+import TheHomepage from'@/components/TheHomepage.vue';
+import AboutMe from '@/components/AboutMe.vue'
+
+export default {
+  components: {
+    TheHomepage,
+    AboutMe
+  }
+}
+</script>
+
+<style>
+
+</style>
