0328674a07e5373d5fd931adb1398e5f4d21b248
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-07-07 12:25:35 +0200

    remove fontawesome component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
 
-// FontAwesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faBrandSecret } from '@fortawesome/free-brands-svg-icons'
-library.add(faBrandSecret)
-
 
 import App from './App.vue';
 import router from './router.js'
@@ -15,7 +9,6 @@ import BaseArrow from './components/ui/BaseArrow.vue'
 const app = createApp(App);
 
 app.component('base-arrow', BaseArrow);
-app.component('font-awesome-icon', FontAwesomeIcon)
 
 
 app.use(router);
