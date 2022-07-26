46debb84043cd41d87eae533302c3391d2f50f3e
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-07-26 15:43:58 +0200

    feat: page error 404

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Erorr 404</h1>
+    <iframe src="https://giphy.com/embed/MwWl4l21R3GvRd4lHx" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
   </div>
 </template>
 
@@ -10,6 +11,28 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+  height: 80vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
 
+h1 {
+  font-family: 'Cinzel', sans-serif;
+  font-size: 5rem;
+  /* display: inline-flex;
+  justify-content: center;
+  align-items: center; */
+}
+
+.giphy-embed {
+  width: auto;
+  /* height: 200px; */
+  min-height: 13.889vw;
+  pointer-events: none;
+}
 </style>
